62dc71cf126ced0d456c79f1596a6230dcdb006e
dafiq856 <dafiq856@gmail.com>
2021-09-13 08:35:29 +0800

    commit 5

--- static/js/main.js
+++ static/js/main.js
@@ -8,6 +8,26 @@ function showPassword() {
   }
 
 
+// Get the container element
+var btnContainer = document.getElementById("myDiv");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav__link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
 // Get the modal
 // var modal = document.getElementById("myModal");
 
